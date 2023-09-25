661cc82586f8302d7af4483a5ef585b66b280429
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-09-25 21:07:01 +0300

    перемножил элементы массива и вывел результат в консоль

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let num = 266219;
-let array = ("" + num).split("");
+let array = ("" + num).split("").map(Number);
+let initVal = 1;
+for (let i = 0; i < array.length; ++i) {
+  initVal *= array[i];
+}
 console.log(array);
+console.log(initVal);
