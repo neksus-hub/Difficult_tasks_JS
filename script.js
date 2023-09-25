1ba8cf99cd61c93485f8aa15319aaf5e39819b6b
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-09-25 21:35:28 +0300

    вывел в консоль первые 2 символа числа и дописал комменты

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let num = 266219;
-let array = ("" + num).split("").map(Number);
-let initVal = 1;
+let array = ("" + num).split("").map(Number); // разделил число на массив цифр
+let initVal = 1; // стартовое значение массива
 for (let i = 0; i < array.length; ++i) {
-  initVal *= array[i];
+  // обозначил массив
+  initVal *= array[i]; // на каждой итерации умножаю эл-т массива на предыдущий
 }
-console.log(array);
-console.log(initVal);
-initVal **= 3;
-console.log(initVal);
-console.log(typeof initVal);
+console.log(array); // вывел массив
+console.log(initVal); // вывел результат умножения эл-тов массива
+initVal **= 3; // возвел в 3 степень
+console.log(initVal); // вывел р-т умн-я в 3-ую степень
+console.log(String(initVal).slice(0, 2)); // вывел первые два эл-та плученного числа
