f1810e257179eee265fec85b3a8d9df0c68231a6
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-02 00:04:11 +0300

    вывел в консоль простые числа от 1 до 100 и его делители в столбик

--- lesson05.js
+++ lesson05.js
@@ -1,4 +1,5 @@
 let arr = ["245", "424", "123123", "223123", "324324", "34566", "657757"];
+const n = 100;
 
 console.log(arr);
 
@@ -6,5 +7,15 @@ for (let i = 0; i < arr.length; i++) {
   broken = arr[i].split("");
   if (arr[i].startsWith("2") || arr[i].startsWith("4")) {
     console.log(arr[i]);
+  }
+}
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 2; j <= i; j++) {
+    if (i % j === 0 && j < i) {
+      break;
+    } else if (j === i) {
+      console.log("Простое число: " + i + " Делители: " + "1" + " и " + j);
+    }
   }
 }
