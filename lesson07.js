ddbca47e49195aeae50300fff077f3906adaa072
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-06 23:47:30 +0300

    Написал функцию, которая перебирает массив методом forEach и выводит на экран дни недели с измененной текущей датой и выходными

--- lesson07.js
+++ lesson07.js
@@ -1,1 +1,32 @@
+"use strict";
+const week = [
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+  "Воскресенье",
+];
+const day = document.querySelector(".days");
+const today = new Date();
+const days = function () {
+  week.forEach((item, i) => {
+    let newdiv = document.createElement("div");
+    if (i === +today.getDay() - 1) {
+      console.log(today.getDay());
+      newdiv.classList.add("today");
+      newdiv.textContent = week[i];
+    }
+    if (item == "Суббота" || item == "Воскресенье") {
+      // Если выходные то
+      newdiv.classList.add("italic");
+      newdiv.textContent = week[i];
+    } else {
+      newdiv.textContent = week[i];
+    }
+    day.appendChild(newdiv);
+  });
+};
 
+days();
