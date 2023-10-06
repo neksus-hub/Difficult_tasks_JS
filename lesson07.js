14bf6511a6a3b83b3729b39267ca7271245c8103
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-06 22:24:22 +0300

    clear

--- lesson07.js
+++ lesson07.js
@@ -1,9 +1,1 @@
-function parseStringOrNot(txt) {
-  if (typeof txt !== "string") return "Передана не строка";
-  return txt.trim().length > 30 ? `${txt.trim().slice(0, 30)}...` : txt.trim();
-}
 
-console.log(
-  parseStringOrNot("    Ехал Грека через реку, видит Грека в реке рак     ")
-);
-console.log(parseStringOrNot(10));
